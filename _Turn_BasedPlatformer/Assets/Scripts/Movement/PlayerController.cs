4887d35cc44bafc3a8d3a12b735d4077721d1d9e
CillinIv <45433397+CillinIv@users.noreply.github.com>
2021-03-22 20:27:57 +0000

    Added Player Controller and sound effects
    Sound for wind, death and a few others.

---
+++ _Turn_BasedPlatformer/Assets/Scripts/Movement/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    [SerializeField] float jumpForce = 2f;
+
+    Rigidbody2D rb2d = null;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //Horizontal Movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector2(horizontalInput * speed, VerticalInput * jumpForce)  * Time.deltaTime);
+
+
+    }
+
+}
+
