b07995b30101ec6d7e0e9e88d070bbe50d3c918b
Crotshot <46968070+Crotshot@users.noreply.github.com>
2021-03-22 20:25:30 +0000

    MainMenu Preload Transitions EndScreen(start)

---
+++ _Turn_BasedPlatformer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour{
+    
+
+    public void Awake() {
+        LoadMenu();
+    }
+
+    public void ExitGame(){
+        Application.Quit();
+    }
+
+    public void LoadMenu() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadEndScreen() {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
---
+++ _Turn_BasedPlatformer/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    GameManager gM;
+
+    void Start()
+    {
+        gM = FindObjectOfType<GameManager>();
+    }
+
+    public void StartGameButton() {
+        gM.LoadGame();
+    }
+
+    public void QuitGameButton() {
+        gM.QuitGame();
+    }
+}
