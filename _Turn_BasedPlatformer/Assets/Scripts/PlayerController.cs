f1483d1bcb4e0d9e24ab53347ccbe12fa859fb70
Oisin-Ivory <oisinivory@gmail.com>
2021-03-22 20:25:28 +0000

    Added more sprites for various levels

---
+++ _Turn_BasedPlatformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    [SerializeField] float jumpHeight = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //Get the value of the Horizontal input axis.
+
+        float verticalInput = Input.GetAxis("Vertical");
+        //Get the value of the Vertical input axis.
+
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime);
+
+    }
+}
